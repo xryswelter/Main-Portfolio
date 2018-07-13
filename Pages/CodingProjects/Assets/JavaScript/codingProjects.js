1ba8403960f365e6f8d83944fa893a9952d2722a
Chris Welter <xryswelter@gmail.com>
2018-07-13 12:00:09 -0700

    completed coding page

---
+++ Pages/CodingProjects/Assets/JavaScript/codingProjects.js
@@ -0,0 +1,45 @@
+
+$('button').on('click', function () {
+    console.log($(this).val());
+    let value = $(this).val();
+    // if($(`.${value}`).css('display').toLowerCase()==='none'){
+    //     $(`.${value}`).css('display:block');
+    //     console.log('poo')
+    // } else{
+    //     $(this).css('display:none');
+    //     console.log('shoe');
+    // }
+    switch (value) {
+        case 'unit-4':
+            if ($('#unit-4').css('display') === 'none') {
+                $('#unit-4').css('display', 'block')
+            } else {
+                $('#unit-4').css('display', 'none')
+            }
+            break;
+        case 'trivia':
+        if ($('#trivia').css('display') === 'none') {
+            $('#trivia').css('display', 'block')
+        } else {
+            $('#trivia').css('display', 'none')
+        }
+            break;
+        case 'train':
+        if ($('#train').css('display') === 'none') {
+            $('#train').css('display', 'block')
+        } else {
+            $('#train').css('display', 'none')
+        }
+            break;
+        case 'giphy':
+        if ($('#giphy').css('display') === 'none') {
+            $('#giphy').css('display', 'block')
+        } else {
+            $('#giphy').css('display', 'none')
+        }
+            break;
+
+        default:
+            break;
+    }
+})
