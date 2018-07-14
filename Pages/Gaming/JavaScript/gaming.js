d0c8f9b005f897f3f625cf02c49403fd9adac5d7
Xrys <xryswelter@gmail.com>
2018-07-13 20:12:44 -0700

    finished gaming blog site

---
+++ Pages/Gaming/JavaScript/gaming.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.confirmed').on('click',function(){
+        $('.hidden').css('display','block');
+        $('.disclaimer').css('display','none');
+        
+    })
+})
